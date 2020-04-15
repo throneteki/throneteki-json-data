a3e14cc4787b80d0e9a2d815a26180b0acd05e3a
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2020-04-14 22:34:52 -0700

    rm thronesdb-json-data export tools and instructions. (#82)

--- scripts/export.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const ThronetekiToThronesDbConverter = require('../src/ThronetekiToThronesDbConverter.js');
-
-let exportDir = path.join(process.cwd(), process.argv[2]);
-let converter = new ThronetekiToThronesDbConverter(exportDir);
-converter.convert();
-
-console.log('Updated thronesdb-json-data');
