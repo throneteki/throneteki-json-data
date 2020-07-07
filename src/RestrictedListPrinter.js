23e8d2b6f3312de38bc89de05ad1311e262fc259
Brian Upton <ystros@gmail.com>
2020-07-06 21:27:56 -0700

    Print both joust and melee RLs

--- src/RestrictedListPrinter.js
+++ src/RestrictedListPrinter.js
@@ -10,14 +10,29 @@ class RestrictedListPrinter {
         let output = '';
 
         const currentList = this.restrictedList[this.restrictedList.length - 1];
-        const joustRules = currentList.formats.find(format => format.name === 'joust');
 
-        const restricted = joustRules.restricted.map(code => this.cardData.getCardByCode(code));
-        const banned = (joustRules.banned || []).map(code => this.cardData.getCardByCode(code));
+        output += `${currentList.issuer} v${currentList.version} (${currentList.date})\n`;
+        output += '============================================\n';
+        output += '\n';
+
+        output += this.buildOutputForFormat('joust');
+        output += this.buildOutputForFormat('melee');
+
+        return output;
+    }
+
+    buildOutputForFormat(formatName) {
+        let output = '';
+
+        const currentList = this.restrictedList[this.restrictedList.length - 1];
+        const rules = currentList.formats.find(format => format.name === formatName);
+
+        const restricted = rules.restricted.map(code => this.cardData.getCardByCode(code));
+        const banned = (rules.banned || []).map(code => this.cardData.getCardByCode(code));
 
         const factions = ['thenightswatch', 'greyjoy', 'targaryen', 'tyrell', 'lannister', 'baratheon', 'martell', 'stark', 'neutral'];
 
-        output += `${currentList.issuer} v${currentList.version} (${currentList.date})\n`;
+        output += `${formatName.toUpperCase()}\n`;
         output += '--------------------------------------------\n';
         for(const faction of factions) {
             output += `${faction}:\n`;
@@ -26,7 +41,7 @@ class RestrictedListPrinter {
             for(const card of cards) {
                 output += `${this.formatCard(card)}\n`;
 
-                const pod = joustRules.pods.find(pod => pod.restricted === card.code);
+                const pod = (rules.pods || []).find(pod => pod.restricted === card.code);
                 if(pod) {
                     const podCards = pod.cards.map(code => this.cardData.getCardByCode(code));
                     for(const card of podCards) {
@@ -41,6 +56,8 @@ class RestrictedListPrinter {
         for(const card of banned) {
             output += `${this.formatCard(card)}\n`;
         }
+
+        output += '\n\n';
 
         return output;
     }
